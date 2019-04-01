f83927087b6be6138a292958968e9b8d3cdc0be1
Alan Russell <ajrussellaudio@gmail.com>
2019-04-01 09:32:18 +0100

    add viewport magic

--- pages/_app.js
+++ pages/_app.js
@@ -19,6 +19,12 @@ export default class MyApp extends App {
       <Container>
         <Head>
           <title>Alan's TV Shows</title>
+          <meta charset="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         </Head>
         <Component {...pageProps} />
       </Container>
